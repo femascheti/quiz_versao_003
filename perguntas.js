a5ca23770b95cc24cbfed806983731da8c336533
Fernanda Mascheti <43377020+femascheti@users.noreply.github.com>
2023-11-16 12:08:43 -0300

    update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,58 +1,45 @@
 export const perguntas = [
     {
-        pergunta: "Pergunta 1",
+        pergunta: "Depois de uma aula sobre ferramentas de IA, !!você teve que escrever um trabalho sobre o uso de tecnologia em sala de aula. O que !!você faz? ",
         opcoes: [
             {
-                texto: "Ação 1",
-                afirmacao: ["Afirmação 1",
-                            "Afirmação 2"]
+                texto: "Utilizo uma ferramenta de busca como o Bing Copilot para buscar informações relevantes para meu trabalho.",
+                afirmacao: ["Mantém a curiosidade ao uso de ferramentas de IA exercitando o pensamento crítico e a simplificação de tarefas.",
+                            "Consegue utilizar IA para automatizar tarefas simples do dia a dia."]
             },
             {
-                texto: "Ação 2",
-                afirmacao: ["Afirmação 3",
-                            "Afirmação 4"]
-            },
-            {
-                texto: "Ação 3",
-                afirmacao: ["Afirmação 5",
-                            "Afirmação 6"]
+                texto: "Escrevo o trabalho com base nos meus conhecimentos sobre o tema.",
+                afirmacao: ["Ainda sente dificuldade em implantar o uso de IA em tarefas do dia a dia. ",
+                            "Acha que IA vai substituir o trabalho humano e não quer que isso aconteça."]
             },
         ]
     },
     {
-        pergunta: "Pergunta 2",
+        pergunta: "Depois que !!você escreveu seu trabalho, teve uma discussão sobre o impacto da IA no trabalho do futuro !!você:",
         opcoes: [
             {
-                texto: "Ação 1",
-                afirmacao: ["Afirmação 7",
-                            "Afirmação 8"]
+                texto: "Defendo a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas",
+                afirmacao: ["Impulsiona inovações e luta para abrir novos caminhos profissionais com IA.",
+                            "Participou do desenvolvimento de soluções criativas e na melhoria de processos em IA."]
             },
             {
-                texto: "Ação 2",
-                afirmacao: ["Afirmação 9",
-                            "Afirmação 10"]
-            },
-            {
-                texto: "Ação 3",
-                afirmacao: ["Afirmação 11",
-                            "Afirmação 12"]
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendedo a importância de proteger os trabalhadores.",
+                afirmacao: ["Criou grupo de estudos coletivos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                            "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."]
             },
         ]
     },
     {
-        pergunta: "Pergunta 3",
+        pergunta: "Ao final da discussão, !!você precisou criar uma imagem no computador que representasse o que !!você pensa sobre IA.",
         opcoes: [
             {
-                texto: "Ação 1",
-                afirmacao: ["Afirmação 13"]
+                texto: "Crio uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: ["Compartilha seus conhecimentos de design utilizando ferramentas tradicionais incentivando a técnica de pintura digital."]
             },
             {
-                texto: "Ação 2",
-                afirmacao: ["Afirmação 14"]
-            },
-            {
-                texto: "Ação 3",
-                afirmacao: ["Afirmação 15"]
+                texto: "Crio uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: ["Acelerou o processo de criação de trabalhos no futuro utilizando geradores de imagem e agora consegue ensinar pessoas como utilizar também!",
+                            "Compartilha suas artes em suas redes sociais como forma de ensinar como se comunicar através da arte."]
             },
         ]
     },
